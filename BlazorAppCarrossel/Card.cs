0e124009060c594478cbce6292d97dc0ad9d986c
cloudsystems22 <cloudsystems22@gmail.com>
2023-12-26 15:52:48 -0300

    finalização dos componentes de caroussel

---
+++ BlazorAppCarrossel/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppCarrossel
+{
+    public class Card
+    {
+        public int IdCard { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? MarginLeft { get; set; }
+    }
+}
---
+++ BlazorAppCarrossel/ClassCss.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorAppCarrossel
+{
+    public class ClassCss
+    {
+        public int IdSlide { get; set; }
+
+        public string? SlideChildren { get; set; }
+
+        public string? ClassNavManual { get; set; }
+
+        public string? ClassNavAuto { get; set; }
+
+        public string? MarginLeft { get; set; }
+
+        public bool RadioChecked { get; set; }
+    }
+}
